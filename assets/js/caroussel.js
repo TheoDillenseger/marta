be526116a304bedb4c658241c63ee5aea82ac68b
theo <theo.dillenseger@yahoo.fr>
2021-01-06 17:41:56 +0100

    yess

---
+++ assets/js/caroussel.js
@@ -0,0 +1,9 @@
+console.log('hello world');
+/*
+var workContainer = document.getElementsByClassName('WorksContainer')[0];
+console.log(workContainer.childElementCount);
+*/
+var workContainer = document.getElementsByClassName('WorksContainer');
+var listChild = workContainer[0].children;
+console.log(listChild)
+console.log(workContainer);
